ba21e50a4ff5e77061c5226bddf724a17d78dc4b
yashashree <yashashree722@gmail.com>
2025-05-09 15:10:57 +0530

    Describe what you changed heredhghjdh

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,7 +1,32 @@
 from django.db import models
 
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin,BaseUserManager
 # Create your models here.
+class UserProfileManager(BaseUserManager):
+    """Mnager for user Profiles"""
+    def create_user(self,email,name,password=None):
+        """Create a new suer profile"""
+        if not email:
+            raise ValueError ('Must have email address')
+        
+        email = self.normalize_email(email)
+        user = self.model(email= email ,name = name)
+        
+        
+        
+        user.set_password(password)
+        user.save(using =self._db)
+        return user
+    
+    def create_superuser(self,email, name,password):
+        user = self.create_user(email,name,password)
+        if user.is_superuser = True
+        user.is_staff = True
+        
+        user.save(using = self._db)
+        return user
+
+
 class  UserProfile(AbstractBaseUser,PermissionsMixin):
     """ DB model for user cutom model"""
     email = models.EmailField(max_length= 255, unique=True)
