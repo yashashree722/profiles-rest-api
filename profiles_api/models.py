ed9b51ee11b7f4e885a1866d1ffe9c04dac475aa
yashashree <yashashree722@gmail.com>
2025-05-09 14:45:26 +0530

    Describe what you changed heredhghjdh

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 # Create your models here.
+class  UserProfile(AbstractBaseUser,PermissionsMixin):
+    """ DB model for user cutom model"""
+    email = models.EmailField(max_length= 255, unique=True)
+    name = models.CharField(max_length= 100)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    objects = UserProfileManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS =['name' ]
+    
+    def get_full_name(self):
+        """ get the full name of user"""
+        return self.name 
+
+    def get_short_name(self):
+        return self.name
+
+         
+    def __str__(self):
+        return self.email 
+    
