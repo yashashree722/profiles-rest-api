d99b18ad10e5214ada8d2f62a3ab54e2daa71eaa
yashashree <yashashree722@gmail.com>
2025-05-11 15:00:28 +0530

    ApiView

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from profiles_api import views
 
 urlpatterns = [
-  path('hello-view/' , views.Hello_api_view.as_view()),
+  # path('hello-view/' , views.Hello_api_view.as_view()),
 ]
